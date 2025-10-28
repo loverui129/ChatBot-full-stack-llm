1427c9db5827b76760403de80ec777ccd74fddb3
RUI ZHANG <167585985+loverui129@users.noreply.github.com>
2025-10-28 16:42:28 -0700

    Update init_db.py

--- backend/database/init_db.py
+++ backend/database/init_db.py
@@ -1,6 +1,6 @@
 # backend/database/init_db.py
-from backend.database.connection import Base, engine
-from backend.models.document import Document 
+from database.connection import Base, engine
+from models.document import Document 
 
 def init_db():
     Base.metadata.create_all(bind=engine)
